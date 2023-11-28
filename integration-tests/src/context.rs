2917ad6bd9094932b39fb86e2eea794b33ad503a
Vladas <146100@gmail.com>
2023-11-28 11:54:33 +0200

    added multisig and ft contract integration interfaces

--- integration-tests/src/context.rs
+++ integration-tests/src/context.rs
@@ -3,7 +3,9 @@
 use anyhow::Result;
 use async_trait::async_trait;
 use integration_utils::integration_contract::IntegrationContract;
+use multisig_integration::{Multisig, MULTISIG};
 use near_workspaces::Account;
+use sweat_integration::{SweatFt, FT_CONTRACT};
 
 use crate::lockup_interface::{LockupContract, LOCKUP_CONTRACT};
 
@@ -15,6 +17,8 @@ pub trait IntegrationContext {
     async fn alice(&mut self) -> Result<Account>;
     async fn fee(&mut self) -> Result<Account>;
     fn lockup(&self) -> LockupContract<'_>;
+    fn multisig(&self) -> Multisig<'_>;
+    fn ft_contract(&self) -> SweatFt<'_>;
 }
 
 #[async_trait]
@@ -34,10 +38,18 @@ impl IntegrationContext for Context {
     fn lockup(&self) -> LockupContract<'_> {
         LockupContract::with_contract(&self.contracts[LOCKUP_CONTRACT])
     }
+
+    fn multisig(&self) -> Multisig<'_> {
+        Multisig::with_contract(&self.contracts[MULTISIG])
+    }
+
+    fn ft_contract(&self) -> SweatFt<'_> {
+        SweatFt::with_contract(&self.contracts[FT_CONTRACT])
+    }
 }
 
 pub(crate) async fn prepare_contract() -> Result<Context> {
-    let context = Context::new(&[LOCKUP_CONTRACT], "build-integration".into()).await?;
+    let context = Context::new(&[LOCKUP_CONTRACT, MULTISIG, FT_CONTRACT], "build-integration".into()).await?;
     //context.lockup().new().await?;
     Ok(context)
 }
--- integration-tests/src/lockup_interface.rs
+++ integration-tests/src/lockup_interface.rs
@@ -179,7 +179,7 @@ impl<'a> IntegrationContract<'a> for LockupContract<'a> {
         }
     }
 
-    fn with_user(mut self, account: &Account) -> Self {
+    fn with_user(&mut self, account: &Account) -> &mut Self {
         self.account = account.clone().into();
         self
     }
