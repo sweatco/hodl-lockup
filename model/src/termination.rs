ae6359ffa18ec1423459570759a3310bc46b216a
Vasily Styagov <styagov.dev@gmail.com>
2025-08-01 00:54:29 +0300

    feat: add retroactive lockup termination

--- model/src/termination.rs
+++ model/src/termination.rs
@@ -43,10 +43,22 @@ impl Lockup {
             VestingConditions::Schedule(schedule) => schedule,
         }
         .unlocked_balance(termination_timestamp);
+
+        let (vested_balance, termination_timestamp) = if vested_balance >= self.claimed_balance {
+            println!("@@ Use data as is");
+            (vested_balance, termination_timestamp)
+        } else {
+            let last_claim_timestamp = self.schedule.get_vesting_timestamp_for_amount(vested_balance);
+            println!("@@ Adjust data");
+            (self.claimed_balance, last_claim_timestamp)
+        };
+
         let unvested_balance = total_balance - vested_balance;
         if unvested_balance > 0 {
             self.schedule.terminate(vested_balance, termination_timestamp);
         }
+
+        dbg!(self.schedule.clone());
         (unvested_balance, termination_config.beneficiary_id)
     }
 }
