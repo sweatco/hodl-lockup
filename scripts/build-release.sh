7e7a2bda17cccc675ebd32f31def47d7f8c2794e
Vasily Styagov <styagov.dev@gmail.com>
2026-07-30 11:47:52 +0100

    fix: release script

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -8,5 +8,6 @@ docker run \
      --mount type=bind,source=$HOST_DIR,target=/host \
      --cap-add=SYS_PTRACE \
      --security-opt seccomp=unconfined \
-     -t nearprotocol/contract-builder:latest-amd64 \
+     --platform linux/amd64 \
+     -t sourcescan/cargo-near:0.13.4-rust-1.85.0@sha256:a9d8bee7b134856cc8baa142494a177f2ba9ecfededfcdd38f634e14cca8aae2 \
      /bin/bash -c "cd /host && make build"
